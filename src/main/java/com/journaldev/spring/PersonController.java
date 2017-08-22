e32a16773df962d22979026cf1ee54a406c660b8
srinivashyd77 <srinivashyd77@gmail.com>
2017-08-22 21:51:59 +0530

    Update PersonController.java
    sop is modified again directly

--- src/main/java/com/journaldev/spring/PersonController.java
+++ src/main/java/com/journaldev/spring/PersonController.java
@@ -25,7 +25,7 @@ public class PersonController {
 	
 	@RequestMapping(value = "/persons", method = RequestMethod.GET)
 	public String listPersons(Model model) {
-		System.out.println("from PersonController listPersons()");
+		System.out.println("from PersonController listPersons() new");
 		model.addAttribute("person", new Person());
 		model.addAttribute("listPersons", this.personService.listPersons());
 		return "person";
